19daeb59507e2853a7c2a010fe6bd1f3e55e5144
Dmytro Khevpa <khevpa.dmytro@gmail.com>
2025-01-10 00:58:20 +0200

    System tray app added, pyinstall build added

--- main.py
+++ main.py
@@ -1,17 +1,41 @@
 from pywinauto import Application
+from PIL import Image
+
+import pystray
 
 from core.hotkey_manager import HotkeyManager
 from modules.terrain_edit_tool import TerrainEditModule
 from modules.vegetation_edit_tool import VegetationEditModule
 
 
+def on_clicked_toggle_autorun(tray, item):
+    pass
+
+
+def on_clicked_exit(tray, item):
+    tray.stop()
+
+
+def setup_tray_icon():
+    icon = Image.open("icon.png")
+    menu = pystray.Menu(
+        pystray.MenuItem("Settings", on_clicked_exit),
+        pystray.MenuItem("Auto-launch", on_clicked_toggle_autorun),
+        pystray.MenuItem("Exit", on_clicked_exit)
+    )
+
+    tray = pystray.Icon("Dijkstra", icon, "REDkit hotkey extender", menu)
+    tray.run_detached()
+
+
 def main():
     m = HotkeyManager()
-
     a = Application().connect(title_re=".*REDkit.*")
 
     TerrainEditModule(a, m)
     VegetationEditModule(a, m)
+
+    setup_tray_icon()
 
     m.start_listening()
 
